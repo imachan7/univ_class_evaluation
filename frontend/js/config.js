005f1b3c9035e918087531b32ea0756a71c6bd87
imachan7 <mr_ima_kino@yahoo.co.jp>
2026-03-18 09:34:28 +0100

    Move frontend files to frontend directory and update API URL

---
+++ frontend/js/config.js
@@ -0,0 +1,2 @@
+// バックエンドのURLをここだけで管理する
+const API_BASE_URL = "https://univ-class-api-azdsdng5ena4ajc9.japaneast-01.azurewebsites.net";
--- js/config.js
+++
@@ -1,4 +0,0 @@
-// バックエンドのIPアドレスをここだけで管理する
-// localhostで動かす場合: "http://localhost:3000"
-// LAN公開する場合: "http://<PCのIPアドレス>:3000"
-const API_BASE_URL = "http://10.3.218.87:3000";
